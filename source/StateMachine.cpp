b503db08450dd85f08bd6c9d4f1e0e6ea73c4ba9
Godofdrakes <Godofdrakes@gmail.com>
2014-09-30 14:03:23 -0700

    Defined StateMachine

--- source/StateMachine.cpp
+++ source/StateMachine.cpp
@@ -5,3 +5,39 @@ StateMachine::StateMachine(void) {}
 
 
 StateMachine::~StateMachine(void) {}
+
+void StateMachine::PushState(BaseState* gameState) {
+	stateStack.pushback(gameState);
+	gameState->Init();
+
+}
+
+BaseState* StateMachine::PopState() {
+	BaseState* lastState = stateStack.Back();
+	stateStack.pop_back();
+	lastState->Destroy();
+	return lastState;
+
+}
+
+BaseState* StateMachine::SwitchState(BaseState* gameState) {
+	BaseState* lastState = stateStack.back();
+	lastState->Destroy();
+
+	stateStack.pop_back();
+	stateStack.push_back(gameState);
+	gameState->Init();
+
+	return lastState;
+
+}
+
+void StateMachine::Update(float deltaTime) {
+	stateStack.back()->Update(deltaTime, this);
+
+}
+
+void StateMachine::Draw() {
+	stateStack.back->Draw();
+
+}
