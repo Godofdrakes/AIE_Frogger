f0283c4dc911ec3b39372f4eda0c29470ca01dae
Godofdrakes <Godofdrakes@gmail.com>
2014-10-15 11:59:40 -0700

    Updated draw and exit from settingsstate

--- source/SettingsState.cpp
+++ source/SettingsState.cpp
@@ -16,12 +16,15 @@ void SettingsState::Init() {
 }
 
 void SettingsState::Update(float deltaTime, StateMachine* a_pSM) {
-
+	if (IsKeyDown(GLFW_KEY_TAB)) { delete a_pSM->PopState(); return; }
 
 }
 
 void SettingsState::Draw() {
-
+	for(auto mapTile : mapTiles) { // Iterate through every map tile
+		MoveSprite( mapTile->SpriteID(), mapTile->X(), mapTile->Y() );
+		DrawSprite( mapTile->SpriteID() );
+	}
 
 }
 
